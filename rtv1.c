3b465902a34ee39dd3dd8a4a31e33ccb6548ae40
dotline <dotline@dotline.com>
2017-03-16 18:00:42 +0200

    Update.

--- rtv1.c
+++ rtv1.c
@@ -12,11 +12,9 @@ int		intersection(t_ray *ray, double *t, t_sphere *sphere)
 	ft_vecinit(ray->origin->x, ray->origin->y, ray->origin->z, temp_o);
 	ft_vecinit(ray->direction->x, ray->direction->y, ray->direction->z, temp_d);
 	ft_vecdiff(temp_o, sphere->center, temp_oc);
-
 	double b = 2 * ft_vecdot(temp_oc, temp_d);
 	double c = ft_vecdot(temp_oc, temp_oc) - sphere->radius * sphere->radius;
 	double disc = b * b - 4 * c;
-
 	free(temp_o);
 	free(temp_d);
 	free(temp_oc);
@@ -38,30 +36,30 @@ void	ft_render(t_wnd *ws)
 	int			y;
 	int			x;
 	t_ray		*ray;
-	t_vec		*orig = (t_vec*)malloc(sizeof(t_vec));
-	t_vec		*dir = (t_vec*)malloc(sizeof(t_vec));
-	t_sphere	*sphere = (t_sphere*)malloc(sizeof(t_vec));
-	t_vec		*center = (t_vec*)malloc(sizeof(t_vec));
+	t_vec		*orig;
+	t_vec		*dir;
+	t_sphere	*sphere;
+	t_vec		*center;
 
+	orig = (t_vec*)malloc(sizeof(t_vec));
+	dir = (t_vec*)malloc(sizeof(t_vec));
+	sphere = (t_sphere*)malloc(sizeof(t_vec));
+	center = (t_vec*)malloc(sizeof(t_vec));
 	ft_vecinit(ws->width / 2, ws->height / 2, 50, center);
 	ft_sphereinit(center, 50, sphere);
-	t = 20000;
 	ray = (t_ray*)malloc(sizeof(t_ray));
+	t = 20000;
 	y = 0;
 	while (y < ws->height)
 	{
 		x = 0;
 		while (x < ws->width)
 		{
-			// init vectors and trace ray
 			ft_vecinit(x, y, 0, orig);
 			ft_vecinit(0, 0, 1, dir);
 			ft_rayinit(orig, dir, ray);
-
-			// check intersection
 			if (intersection(ray, &t, sphere) == 1)
 			{
-				// put_pixel
 				mlx_pixel_put(ws->mlx, ws->win, x, y, 0xFFFFFF);
 			}
 			x++;
@@ -83,7 +81,7 @@ int		main(int argc, char *argv[])
 	ws->width = 500;
 	ws->height = 500;
 	ws->mlx = mlx_init();
-	ws->win = mlx_new_window(ws->mlx, ws->width, ws->height, "Test");
+	ws->win = mlx_new_window(ws->mlx, ws->width, ws->height, "RayTrace");
 	ft_render(ws);
 	mlx_loop(ws->mlx);
 	return (0);
