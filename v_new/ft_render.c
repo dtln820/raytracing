cf7b1072ee314bd36b5d3d23874a96a5e2eb6c9a
Dimitrii DULGHER <ddulgher@e1p72.chisinau.42.fr>
2017-04-13 16:24:07 +0300

    ft_trace

--- v_new/ft_render.c
+++ v_new/ft_render.c
@@ -1,5 +1,8 @@
 #include "hExtract_info.h"
 #include "hPrint.h"
+#include <float.h>
+
+const double INFINITY = DBL_MAX;
 
 int		ft_sumobjects()
 {
@@ -10,31 +13,105 @@ int		ft_sumobjects()
 	return (result);
 }
 
-void	ft_trace()
+_Bool	ft_trace()
 {
-	int		x;
-	int		y;
+	int			obj_nr;
+	int			i;
+	int			cur_obj;
+	t_sphnode	*sph_sent;
+	t_planenode	*plane_sent;
+	t_cylnode	*cyl_sent;
+	t_conenode	*cone_sent;
 
-	x = 0;
-	y = 0;
-	while (x < WIDTH)
+	obj_nr = ft_sumobjects();
+	i = 0;
+	sph_sent = sph_head;
+	plane_sent = plane_head;
+	cyl_sent = cyl_head;
+	cone_sent = cone_head;
+	while (i < obj_nr)
 	{
-		while (y < HEIGHT)
+		double tNearK = INFINITY;
+		uint32_t indexK;
+		Vec2f uvK;
+		while (sph_sent != NULL)
 		{
-			
+			if (ft_sphintersect(orig, dir, tNearK, indexK, uvK, sph_sent))
+				if (tNearK < tNear)
+				{
+					tNear = tNearK;
+					index = indexK;
+					uv = uvK;
+					// do some shit
+				}
+			sph_sent = sph_sent->next;
 		}
+		while (plane_sent != NULL)
+		{
+			if (ft_planeintersect(orig, dir, tNearK, indexK, uvK, plane_sent))
+				if (tNearK < tNear)
+				{
+					tNear = tNearK;
+					index = indexK;
+					uv = uvK;
+					// do some shit
+				}
+			plane_sent = plane_sent->next;
+		}
+		while (cyl_sent != NULL)
+		{
+			if (ft_cylintersect(orig, dir, tNearK, indexK, uvK, cyl_sent))
+				if (tNearK < tNear)
+				{
+					tNear = tNearK;
+					index = indexK;
+					uv = uvK;
+					// do some shit
+				}
+			cyl_sent = cyl_sent->next;
+		}
+		while (cone_sent != NULL)
+		{
+			if (ft_coneintersect(orig, dir, tNearK, indexK, uvK, cone_sent))
+				if (tNearK < tNear)
+				{
+					tNear = tNearK;
+					index = indexK;
+					uv = uvK;
+					// do some shit
+				}
+			cone_sent = cone_sent->next;
+		}
+	}
+}
+
+int		ft_castray()
+{
+	if (ft_trace())
+	{
+		/*
+		Do the math
+		*/
 	}
 }
 
 void	ft_render()
 {
-	int		total_objects;
-	int		contor;
+	int		color;
+	int		j;
+	int		i;
 
-	total_objects = ft_sumobjects();
-	contor = 0;
-	while (contor < total_objects)
+	j = 0;
+	while (j < HEIGHT)
 	{
-		ft_trace(); // ? ? ?
+		i = 0;
+		while (i < WIDTH)
+		{
+			/*
+			Generate ray
+			Calculate the direction vector
+			*/
+			color = ft_castray();
+		}
 	}
 }
